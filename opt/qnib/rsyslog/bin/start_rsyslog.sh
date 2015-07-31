53957d43676e0d8316279edba5cfd338ed268cfe
Christian Kniep <christian@qnib.org>
2015-07-31 11:04:38 +0200

    stop rsyslog by trap SIGTERM

--- opt/qnib/rsyslog/bin/start_rsyslog.sh
+++ opt/qnib/rsyslog/bin/start_rsyslog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+trap "echo stopping rsyslog;kill -9 $(cat /var/run/rsyslogd.pid); exit" HUP INT TERM EXIT
+
 DEFAULT=true
 if [ "X${FORWARD_TO_LOGSTASH}" == "Xtrue" ];then
    ln -sf /etc/rsyslog.d/logstash.conf.disabled /etc/rsyslog.d/logstash.conf  
@@ -16,4 +18,8 @@ fi
 if [ ${DEFAULT} == "true" ];then
    ln -sf /etc/rsyslog.d/file.conf.disabled /etc/rsyslog.d/file.conf  
 fi
-/usr/sbin/rsyslogd -n
+/usr/sbin/rsyslogd 
+while true
+do
+  sleep 1
+done
