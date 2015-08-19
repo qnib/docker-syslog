315981ae19a0126d5d225096297abaae7d6c0719
Christian Kniep <christian@qnib.org>
2015-08-19 23:56:52 +0200

    add heka forwarder (using TcpInput of heka

--- opt/qnib/rsyslog/bin/start_rsyslog.sh
+++ opt/qnib/rsyslog/bin/start_rsyslog.sh
@@ -20,6 +20,10 @@ if [ "X${FORWARD_TO_KAFKA}" == "Xtrue" ];then
    ln -sf /etc/rsyslog.d/kafka.conf.disabled /etc/rsyslog.d/kafka.conf  
    DEFAULT=false
 fi
+if [ "X${FORWARD_TO_HEKA}" == "Xtrue" ];then
+   ln -sf /etc/rsyslog.d/heka.conf.disabled /etc/rsyslog.d/heka.conf  
+   DEFAULT=false
+fi
 if [ "X${FORWARD_TO_FILE}" == "Xtrue" ];then
    if [ ! -f /etc/rsyslog.d/file.conf ];then
        ln -sf /etc/rsyslog.d/file.conf.disabled /etc/rsyslog.d/file.conf  
